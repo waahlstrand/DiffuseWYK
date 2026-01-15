0da8684790539743e3856d01997538223a0df87d
Victor Wåhlstrand Skärström <victor.wahlstrand@gmaiil.com>
2026-01-15 17:12:16 +0100

    Updated ddim sampling

--- diffusionwyk/detector.py
+++ diffusionwyk/detector.py
@@ -10,6 +10,7 @@ import random
 from typing import List
 from collections import namedtuple
 
+from matplotlib import patches
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -18,11 +19,15 @@ from detectron2.layers import batched_nms
 from detectron2.modeling import META_ARCH_REGISTRY, build_backbone, detector_postprocess
 
 from detectron2.structures import Boxes, ImageList, Instances
+from detectron2.utils.visualizer import ColorMode, Visualizer
+import cv2
 
 from .loss import SetCriterionDynamicK, HungarianMatcherDynamicK
 from .head import DynamicHead
-from .util.box_ops import box_cxcywh_to_xyxy, box_xyxy_to_cxcywh
+from .util.box_ops import box_cxcywh_to_xyxy, box_xyxy_to_cxcywh, box_normalize_xyxy
 from .util.misc import nested_tensor_from_tensor_list
+
+import matplotlib.pyplot as plt
 
 __all__ = ["DiffusionWYK"]
 
@@ -643,7 +648,7 @@ class DiffusionWYK(DiffusionDetBase):
         )  # Number of known labels at test time
 
     @torch.no_grad()
-    def ddim_sample(
+    def ddim_sample_old(
         self,
         batched_inputs,
         backbone_feats,
@@ -794,6 +799,7 @@ class DiffusionWYK(DiffusionDetBase):
             box_cls = output["pred_logits"]
             box_pred = output["pred_boxes"]
             results = self.inference(box_cls, box_pred, images.image_sizes)
+
         if do_postprocess:
             processed_results = []
             for results_per_image, input_per_image, image_size in zip(
@@ -833,12 +839,11 @@ class DiffusionWYK(DiffusionDetBase):
 
         # Prepare Proposals.
         if not self.training:
-            # print("Eval", batched_inputs[0].keys())
             results = self.ddim_sample(batched_inputs, features, images_whwh, images)
+
             return results
 
         if self.training:
-            # print("Train", batched_inputs[0].keys())
             gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
             targets, x_boxes, noises, t = self.prepare_targets(gt_instances)
             t = t.squeeze(-1)
@@ -896,6 +901,9 @@ class DiffusionWYK(DiffusionDetBase):
 
     def prepare_diffusion_concat(self, gt_boxes):
         """
+        For each image, randomly select a number of known boxes (up to num_known_train),
+        keep them fixed during diffusion, and diffuse the rest boxes.
+
         :param gt_boxes: (cx, cy, w, h), normalized
         :param num_proposals:
         """
@@ -975,3 +983,193 @@ class DiffusionWYK(DiffusionDetBase):
         diff_boxes = box_cxcywh_to_xyxy(x)
 
         return diff_boxes, noise, t
+
+    @torch.no_grad()
+    def ddim_sample(
+        self,
+        batched_inputs,
+        backbone_feats,
+        images_whwh,
+        images,
+        clip_denoised=True,
+        do_postprocess=True,
+    ):
+        batch = images_whwh.shape[0]
+        shape = (batch, self.num_proposals, 4)
+        total_timesteps, sampling_timesteps, eta, objective = (
+            self.num_timesteps,
+            self.sampling_timesteps,
+            self.ddim_sampling_eta,
+            self.objective,
+        )
+
+        # [-1, 0, 1, 2, ..., T-1] when sampling_timesteps == total_timesteps
+        times = torch.linspace(-1, total_timesteps - 1, steps=sampling_timesteps + 1)
+        times = list(reversed(times.int().tolist()))
+        time_pairs = list(
+            zip(times[:-1], times[1:])
+        )  # [(T-1, T-2), (T-2, T-3), ..., (1, 0), (0, -1)]
+
+        img = torch.randn(shape, device=self.device)
+
+        # Get known boxes from batched_inputs via the 'instances' and 'known_mask' field
+
+        if self.num_known_test > 0:
+            known_boxes_batch = []
+            for i, bi in enumerate(batched_inputs):
+
+                assert (
+                    "instances" in bi
+                ), "Input batch must contain 'instances' with 'known_mask' field for known boxes."
+                instances = bi["instances"]
+
+                assert hasattr(
+                    instances, "known_mask"
+                ), "Instances must have 'known_mask' field for known boxes."
+                known_mask = instances.known_mask
+                known_boxes = instances.gt_boxes.tensor[known_mask].to(self.device)
+                known_boxes_batch.append(known_boxes)
+        else:
+            # Add empty known boxes if no known boxes are specified
+            known_boxes_batch = [
+                torch.empty((0, 4), device=self.device) for _ in range(batch)
+            ]
+
+        known_boxes_batch = torch.stack(known_boxes_batch)
+
+        # Insert known boxes into the initial noisy image
+        if self.num_known_test > 0:
+            for i in range(batch):
+                known_boxes = known_boxes_batch[i]
+
+                # Convert known boxes to model coordinate space
+                known_boxes_normalized = box_normalize_xyxy(
+                    known_boxes, w=images_whwh[i, 0], h=images_whwh[i, 1]
+                )
+                known_boxes = box_xyxy_to_cxcywh(known_boxes_normalized)
+                # Move to model coordinate space
+                known_boxes = (known_boxes * 2.0 - 1.0) * self.scale
+
+                num_known_boxes = known_boxes.shape[0]
+                if num_known_boxes > 0:
+
+                    # Insert known boxes into the initial noisy set
+                    img[i, :num_known_boxes, :] = known_boxes
+
+        ensemble_score, ensemble_label, ensemble_coord = [], [], []
+        x_start = None
+
+        for time, time_next in time_pairs:
+
+            time_cond = torch.full((batch,), time, device=self.device, dtype=torch.long)
+            self_cond = x_start if self.self_condition else None
+
+            preds, outputs_class, outputs_coord = self.model_predictions(
+                backbone_feats,
+                images_whwh,
+                img,
+                time_cond,
+                self_cond,
+                clip_x_start=clip_denoised,
+            )
+            pred_noise, x_start = preds.pred_noise, preds.pred_x_start
+
+            # Set predictions for known boxes to zero noise and original boxes
+            if self.num_known_test > 0:
+                for i in range(batch):
+                    known_boxes = known_boxes_batch[i]
+                    num_known_boxes = known_boxes.shape[0]
+                    if num_known_boxes > 0:
+                        pred_noise[i, :num_known_boxes, :] = 0.0
+                        x_start[i, :num_known_boxes, :] = known_boxes
+
+            if self.box_renewal:  # filter
+                score_per_image, box_per_image = (
+                    outputs_class[-1][0],
+                    outputs_coord[-1][0],
+                )
+                threshold = 0.5
+                score_per_image = torch.sigmoid(score_per_image)
+                value, _ = torch.max(score_per_image, -1, keepdim=False)
+                keep_idx = value > threshold
+
+                # Make sure the known boxes are always kept
+                if self.num_known_test > 0:
+                    num_known_boxes = known_boxes_batch[0].shape[0]
+                    if num_known_boxes > 0:
+                        keep_idx[:num_known_boxes] = True
+
+                num_remain = torch.sum(keep_idx)
+
+                pred_noise = pred_noise[:, keep_idx, :]
+                x_start = x_start[:, keep_idx, :]
+                img = img[:, keep_idx, :]
+            if time_next < 0:
+                img = x_start
+                continue
+
+            alpha = self.alphas_cumprod[time]
+            alpha_next = self.alphas_cumprod[time_next]
+
+            sigma = (
+                eta * ((1 - alpha / alpha_next) * (1 - alpha_next) / (1 - alpha)).sqrt()
+            )
+            c = (1 - alpha_next - sigma**2).sqrt()
+
+            noise = torch.randn_like(img)
+
+            img = x_start * alpha_next.sqrt() + c * pred_noise + sigma * noise
+
+            if self.box_renewal:  # filter
+                # replenish with randn boxes
+                img = torch.cat(
+                    (
+                        img,
+                        torch.randn(
+                            1, self.num_proposals - num_remain, 4, device=img.device
+                        ),
+                    ),
+                    dim=1,
+                )
+            if self.use_ensemble and self.sampling_timesteps > 1:
+                box_pred_per_image, scores_per_image, labels_per_image = self.inference(
+                    outputs_class[-1], outputs_coord[-1], images.image_sizes
+                )
+                ensemble_score.append(scores_per_image)
+                ensemble_label.append(labels_per_image)
+                ensemble_coord.append(box_pred_per_image)
+
+        if self.use_ensemble and self.sampling_timesteps > 1:
+            box_pred_per_image = torch.cat(ensemble_coord, dim=0)
+            scores_per_image = torch.cat(ensemble_score, dim=0)
+            labels_per_image = torch.cat(ensemble_label, dim=0)
+            if self.use_nms:
+                keep = batched_nms(
+                    box_pred_per_image, scores_per_image, labels_per_image, 0.5
+                )
+
+                box_pred_per_image = box_pred_per_image[keep]
+                scores_per_image = scores_per_image[keep]
+                labels_per_image = labels_per_image[keep]
+
+            result = Instances(images.image_sizes[0])
+            result.pred_boxes = Boxes(box_pred_per_image)
+            result.scores = scores_per_image
+            result.pred_classes = labels_per_image
+            results = [result]
+        else:
+            output = {"pred_logits": outputs_class[-1], "pred_boxes": outputs_coord[-1]}
+            box_cls = output["pred_logits"]
+            box_pred = output["pred_boxes"]
+            results = self.inference(box_cls, box_pred, images.image_sizes)
+        if do_postprocess:
+            processed_results = []
+            for results_per_image, input_per_image, image_size in zip(
+                results, batched_inputs, images.image_sizes
+            ):
+                height = input_per_image.get("height", image_size[0])
+                width = input_per_image.get("width", image_size[1])
+                r = detector_postprocess(results_per_image, height, width)
+                processed_results.append({"instances": r})
+
+            return processed_results
