46e17aeb4c26bfabf2adcc0c7d56b74f6013ecc4
Victor Wåhlstrand Skärström <victor.wahlstrand@gmaiil.com>
2026-02-11 09:13:09 +0100

    Added lvis

--- diffusionwyk/dataset_mapper.py
+++ diffusionwyk/dataset_mapper.py
@@ -192,3 +192,39 @@ def register_coco_instances_diffusionwyk(name, metadata, json_file, image_root):
     MetadataCatalog.get(name).set(
         json_file=json_file, image_root=image_root, evaluator_type="coco", **metadata
     )
+
+
+def register_lvis_instances_diffusionwyk(name, metadata, json_file, image_root):
+    """
+    Register a dataset in LVIS's json annotation format for
+    instance detection and instance segmentation.
+    (i.e., Type 1 in http://cocodataset.org/#format-data.
+    `instances*.json` in the dataset).
+
+    This is an example of how to register a new dataset.
+    You can do something similar to this function, to register new datasets.
+
+    Args:
+        name (str): the name that identifies a dataset, e.g. "lvis_v1_train".
+        metadata (dict): extra metadata associated with this dataset.  You can
+            leave it as an empty dict.
+        json_file (str): path to the json instance annotation file.
+        image_root (str or path-like): directory which contains all the images.
+    """
+    assert isinstance(name, str), name
+    assert isinstance(json_file, (str, os.PathLike)), json_file
+    assert isinstance(image_root, (str, os.PathLike)), image_root
+    # 1. register a function which returns dicts
+    if name not in DatasetCatalog:
+        DatasetCatalog.register(
+            name,
+            lambda: load_coco_json(
+                json_file, image_root, name, extra_annotation_keys=["known"]
+            ),
+        )
+
+    # 2. Optionally, add metadata about this dataset,
+    # since they might be useful in evaluation, visualization or logging
+    MetadataCatalog.get(name).set(
+        json_file=json_file, image_root=image_root, evaluator_type="lvis", **metadata
+    )
